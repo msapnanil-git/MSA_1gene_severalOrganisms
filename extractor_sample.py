356373e55fb553fe58aa621d0d1f1d0ccab83171
Sapnanil M. <m.sapnanil@gmail.com>
2025-08-31 01:29:15 +0530

    comment update extractor_sample.py

--- extractor_sample.py
+++ extractor_sample.py
@@ -1,9 +1,10 @@
 from Bio import Entrez, SeqIO
 import os
 import time
-
 # Entrez for NCBI access; SeqIO for sequence file operations; os for directories; time for rate limiting
+
 Entrez.email = "example@gmail.com"
+# provides email for database access
 
 out_dir = "FGF5_MSA"
 # gives name for folder creation
@@ -24,8 +25,10 @@ for org in organisms:
 
     id_list = sr_results["IdList"]
     print(f"\n \033[92mFound \033[0m{len(id_list)} \033[92mrecords with defined nucleotide sequences")
+	# prints the status of the query
 
     for i, ncbi_id in enumerate(id_list, 1):
+	# runs a for loop for aearching all the organisms one by one
         try:
             print(f"\n \033[94mFetching ID: \033[0m{ncbi_id}")
             # print fetching status
@@ -38,13 +41,14 @@ for org in organisms:
             fasta_file = os.path.join(out_dir, f"{org_cl}_{ncbi_id}.fasta")
             with open(fasta_file, "w") as f:
                 SeqIO.write(ft_record, f, "fasta")
-            # creates and saves the FASTA file
+          		# creates and saves the FASTA file
             print(f"\n \033[92mFASTA saved \033[0m{fasta_file}")
-	    # print saving status for FASTA files
+	    	# print saving status for FASTA files
 
             time.sleep(1)
             # intentional delay for complying with server rate limit
 
         except Exception as err:
             print(f"\n \033[91mError with ID \033[0m{ncbi_id}: {err}")
-	    # read and print errors if any
+	   		# read and print errors if any
+
